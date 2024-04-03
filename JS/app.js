b3dd42057e17fed8b8eabc62c5a662b2be736931
AvgColman <116786896+AvgColman@users.noreply.github.com>
2024-04-03 14:59:24 -0400

    added pg code

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+   res.send("Hello! This is the index page")
+});
+
+app.listen(8383, () => {
+   console.log("Listening on port 8383");
+});
+
+
+
+
+
