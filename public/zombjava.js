3eaaccc190e7c7e82a8a60a434fc08c9400bb0a1
AvgColman <116786896+AvgColman@users.noreply.github.com>
2024-04-06 23:14:35 -0400

    working index

--- public/zombjava.js
+++ public/zombjava.js
@@ -17,3 +17,21 @@ fetch('package.json')
     wrappedContent.textContent = storyText;
     storyContainer.appendChild(wrappedContent);
   });
+  $(function(){
+ 
+    $("form").submit(function(){
+       
+        const data = {
+            id: $("#productId").val(),
+            productname: $("#productName").val(),
+            price: $("#productPrice").val()
+        }
+
+        $.post( "/api/product/create", data, function( data ) {
+            console.log("Done");
+        }); 
+ 
+        return false;
+    });
+ });
+ 
--- server.js
+++ server.js
@@ -1,34 +1,55 @@
-
-const express = require('express');
-const path = require('path');
-
-/*
+const express = require("express");
+const bodyParser = require("body-parser");
 const Pool = require('pg').Pool;
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const pool = new Pool({
-  database: 'postgres',
-  server: 'localhost',
-  database: 'postgres',
-  password: '$HarrietPorter24',
-  port: 5432,
-})
-
-app.get("/prices", (req, res) => {
-
-  const sql = "select * from address";
-
-  pool.query(sql, (error, results) => {
-
-      if (error) throw error
-
-      res.status(200).json(results.rows)
-  })
+   user: 'postgres',
+   host: 'localhost',
+   database: 'postgres',
+   password: 'postgres',
+   port: 5432,
 });
-*/
 
-const app = express();
-app.use(express.static("."));
+app.get("/api/product", (req, res) => {
+   const sql = "SELECT * FROM product";
 
-app.listen(8383, function(){
-  console.log("Server running on port 8383")
-})
+   pool.query(sql, (error, results) => {
+       if (error) throw error;
+
+       res.status(200).json(results.rows);
+   });
+});
+
+app.post("/api/product/create", (req, res) => {
+   console.log(req.body);
+
+   const id = req.body.id;
+   const productname = req.body.productname; 
+   const price = req.body.price;
+
+   const sql = "INSERT INTO product (id, productname, price) VALUES ($1, $2, $3)";
+   const data = [id, productname, price]; 
+
+   pool.query(sql, data, (error, results) => {
+       if (error) throw error;
+
+       
+       pool.query('COMMIT', (err) => {
+           if (err) {
+               
+               throw err;
+           }
+           console.log('Transaction completed.');
+       });
+
+       res.status(200).send("ok");
+   });
+});
+
+app.listen(5500, () => {
+   console.log("Listening on port 5500");
+});
